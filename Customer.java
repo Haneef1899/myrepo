042877da038176958a4282ecbf515ed2c36af971
MOHAMMED HANEEF <113276148+Haneef1899@users.noreply.github.com>
2025-04-03 11:11:49 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,9 @@
+package com.tnsif.javaprograms;
+
+public class Customer {
+
+	public static void main(String[] args) {
+		;
+	}
+
+}
---
+++ Javaprog.java
@@ -0,0 +1,19 @@
+package com.tnsif.javaprograms;
+import java.util.*;
+public class Javaprog {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//int newid=sc.nextInt();
+		//System.out.println("Enter the id" +newid1);
+		//System.out.println("Enter the id" +newid1);
+		boolean newid1=sc.hasNextInt();
+		System.out.println("Enter the id" +newid1);
+		if(newid1 == true) {
+			System.out.print(newid1);
+		}
+		
+	}
+
+}
+
